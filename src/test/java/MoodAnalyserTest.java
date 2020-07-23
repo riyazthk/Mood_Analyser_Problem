3a4abc68341b090f94ddb7da078907afd032cf65
riyazthk <er.riyaz2507@gmail.com>
2020-07-23 21:00:16 +0530

    [Riyaz]ADDED:To write a test case for passing null value

--- src/test/java/MoodAnalyserTest.java
+++ src/test/java/MoodAnalyserTest.java
@@ -4,16 +4,25 @@ import org.junit.rules.ExpectedException;
 
 public class MoodAnalyserTest {
     @Test
-    public void testMoodAnalyserSad() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser("This is sad message");
-           String mood = moodAnalyser.analyseMood();
+    public void testMoodAnalyser() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser("sad");
+        String mood = moodAnalyser.analyseMood();
             Assert.assertEquals("sad", mood);
         }
     @Test
     public void testMoodAnalyserHappy() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser("This is happy message");
-        String mood = moodAnalyser.analyseMood();
-        Assert.assertEquals("happy", mood);
+        MoodAnalyser moodAnalyser = new MoodAnalyser("happy");
+          String mood = moodAnalyser.analyseMood();
+            Assert.assertEquals("happy", mood);
+        }
+
+    @Test
+    public void givenNullMoodShouldReturnNull() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser(null);
+          String  mood = moodAnalyser.analyseMood();
+            Assert.assertEquals("happy", mood);
+
     }
     }
+
 
