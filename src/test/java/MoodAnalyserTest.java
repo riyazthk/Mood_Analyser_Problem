b32f1cb4c8d67c2544faa46bce07806ea7e97219
riyazthk <er.riyaz2507@gmail.com>
2020-07-23 20:06:23 +0530

    [Riyaz]ADDED:To Refactor a code passing as paramaterised constructor

--- src/test/java/MoodAnalyserTest.java
+++ src/test/java/MoodAnalyserTest.java
@@ -5,14 +5,14 @@ import org.junit.rules.ExpectedException;
 public class MoodAnalyserTest {
     @Test
     public void testMoodAnalyserSad() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
-           String mood = moodAnalyser.analyseMood("This is sad message");
+        MoodAnalyser moodAnalyser = new MoodAnalyser("This is sad message");
+           String mood = moodAnalyser.analyseMood();
             Assert.assertEquals("sad", mood);
         }
     @Test
     public void testMoodAnalyserHappy() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood = moodAnalyser.analyseMood("This is happy message");
+        MoodAnalyser moodAnalyser = new MoodAnalyser("This is happy message");
+        String mood = moodAnalyser.analyseMood();
         Assert.assertEquals("happy", mood);
     }
     }
