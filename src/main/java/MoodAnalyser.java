79d01a61ef2d403f8e0cb12d1fd4cdf5c43618d1
riyazthk <er.riyaz2507@gmail.com>
2020-07-24 10:04:11 +0530

    [Riyaz]ADDED:To modify a program for a custom exception

--- src/main/java/MoodAnalyser.java
+++ src/main/java/MoodAnalyser.java
@@ -2,18 +2,21 @@ public class MoodAnalyser {
     public String message;
 
     public MoodAnalyser(String message){
-    this.message=message;
+        this.message=message;
 
-}
-    public String analyseMood()  {
+    }
+
+    public String analyseMood() throws MoodAnalysisException {
         try {
+            if(message.length()==0)
+                throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.ENTERED_EMPTY,"please enter proper message");
             if (message.contains("sad")) {
                 return "sad";
             } else {
                 return "happy";
             }
         } catch (NullPointerException e) {
-              return "happy";
+            throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.ENTERED_NULL,"please enter proper message");
         }
     }
 }
