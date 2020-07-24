ee5aee0aec5619c726a60947509e9ce1665b1066
riyazthk <er.riyaz2507@gmail.com>
2020-07-24 10:06:03 +0530

    [Riyaz]ADDED:To create a custom exception

---
+++ src/main/java/MoodAnalysisException.java
@@ -0,0 +1,10 @@
+public class MoodAnalysisException extends Exception {
+    enum ExceptionType{
+        ENTERED_EMPTY,ENTERED_NULL
+    }
+    ExceptionType type;
+    public MoodAnalysisException(ExceptionType type, String message){
+        super(message);
+        this.type=type;
+    }
+}
