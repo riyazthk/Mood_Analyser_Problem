c998a585efbb8d312017b02e9533f5c735bf5976
riyazthk <er.riyaz2507@gmail.com>
2020-07-24 10:29:51 +0530

    [Riyaz]ADDED:To write a text case for to passing empty message

--- src/test/java/MoodAnalyserTest.java
+++ src/test/java/MoodAnalyserTest.java
@@ -4,25 +4,52 @@ import org.junit.rules.ExpectedException;
 
 public class MoodAnalyserTest {
     @Test
-    public void testMoodAnalyser() {
+    public void testMoodAnalyserSad() {
         MoodAnalyser moodAnalyser = new MoodAnalyser("sad");
-        String mood = moodAnalyser.analyseMood();
+        Object mood = null;
+        try {
+            ExpectedException exceptionRule=ExpectedException.none();
+            exceptionRule.expect(MoodAnalysisException.class);
+            mood = moodAnalyser.analyseMood();
             Assert.assertEquals("sad", mood);
+        } catch (MoodAnalysisException e) {
+            e.printStackTrace();
         }
+    }
+
     @Test
     public void testMoodAnalyserHappy() {
         MoodAnalyser moodAnalyser = new MoodAnalyser("happy");
-          String mood = moodAnalyser.analyseMood();
+        Object mood = null;
+        try {
+            ExpectedException exceptionRule=ExpectedException.none();
+            exceptionRule.expect(MoodAnalysisException.class);
+            mood = moodAnalyser.analyseMood();
             Assert.assertEquals("happy", mood);
+        } catch (MoodAnalysisException e) {
+            e.printStackTrace();
         }
+    }
 
     @Test
-    public void givenNullMoodShouldReturnNull() {
+    public void givenNullMoodShouldReturn() {
         MoodAnalyser moodAnalyser = new MoodAnalyser(null);
-          String  mood = moodAnalyser.analyseMood();
-            Assert.assertEquals("happy", mood);
-
+        try {
+            Object mood=moodAnalyser.analyseMood();
+        } catch (MoodAnalysisException e) {
+            Assert.assertEquals(MoodAnalysisException.ExceptionType.ENTERED_NULL,e.type);
+        }
     }
+    @Test
+    public void givenEmptyMoodShouldReturn() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser("");
+        try {
+            Object mood=moodAnalyser.analyseMood();
+        } catch (MoodAnalysisException e) {
+            Assert.assertEquals(MoodAnalysisException.ExceptionType.ENTERED_EMPTY,e.type);
+        }
+    }
+
     }
 
 
